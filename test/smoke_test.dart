dca870a663518a512fd831445998aa007c04a344
vunguyen1412201 <vunguyenhoang2001@gmail.com>
2022-07-03 16:21:26 +0700

    update smoke test

---
+++ test/smoke_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:name_your_price/main.dart';
+
+void main() {
+  testWidgets('smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+  });
+}
